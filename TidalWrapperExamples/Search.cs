23274138605bafdec98817d655d79bc528a98a46
Simon Weyand <simon.weyand@student.uni-siegen.de>
2023-11-04 21:51:09 +0100

    + add example project

---
+++ TidalWrapperExamples/Search.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using TidalWrapper;
+using TidalWrapper.Responses;
+
+namespace SearchDemo
+{
+    public class Search
+    {
+        public static async Task Main(string[] args)
+        {
+            Client client = new("zU4XHVVkc2tDPo4t");
+
+            if (args.Length == 0)
+            {
+                await client.LoginWithDeviceCode(); // Login using device code, which will enter an interactive prompt
+                Console.WriteLine("You can use the following refresh token to login in future:");
+                Console.WriteLine(client.AuthCache?.RefreshToken);
+            }
+            else
+            {
+                await client.LoginWithRefreshToken(args[0]); // Try to login with a provided refresh token
+            }
+
+            Console.Write("Please enter a search query: ");
+            string? query = Console.ReadLine();
+            if (!string.IsNullOrEmpty(query))
+            {
+                TrackSearch results = await client.Tracks.Search(query, 10, client.CountryCode);
+                Console.WriteLine($"=== Received {results.ItemCount} tracks ===");
+                for (int i = 0; i < results.Items.Length; i++)
+                {
+                    Track track = results.Items[i];
+                    Console.WriteLine($"{i + 1}. {track.Title} ({track.Url})");
+                }
+            }
+        }
+    }
+}
